6a702fd062cd50b541c58b15fe81a3c631f19d44
mironovisa <mironovisa@gmail.com>
2023-03-15 13:45:10 +0200

    Updated Milestones 5,6,7

--- js/searchForm.js
+++ js/searchForm.js
@@ -1,32 +1,43 @@
-let searchBoxValue = document.getElementById("searchBox");
-const searchButton = document.getElementById("searchButton");
-const resultsDiv = document.getElementById("resultsDiv");
-const loaderP = document.getElementById("loading");
+class SearchForm {
+  constructor(searchBox, searchButton, loader) {
+    this.searchBox = searchBox;
+    this.searchButton = searchButton;
+    this.loader = loader;
+    this.timeout = 0;
 
-showLoader = () => {
-  loaderP.style.display = "unset";
-};
+    this.searchButton.addEventListener("click", () => {
+      this.autoSearchDebounce();
+      this.queryAddress();
+    });
+    this.searchBox.addEventListener("keyup", () => this.autoSearchDebounce());
+  }
 
-hideLoader = () => {
-  loaderP.style.display = "none";
-};
-queryAddress = () => {
-  window.location.search = `query=${searchBoxValue.value}`;
-};
-let timeout = 0;
+  showLoader() {
+    this.loader.style.display = "unset";
+  }
 
-autoSearchDebounce = () => {
-  clearTimeout(timeout);
-  timeout = setTimeout(async () => {
-    showLoader();
-    await getNameAndSymbol(searchBoxValue.value);
-    hideLoader();
-  }, 500);
-};
+  hideLoader() {
+    this.loader.style.display = "none";
+  }
 
-bothFunctions = () => {
-  autoSearchDebounce();
-};
+  queryAddress() {
+    window.location.search = `query=${this.searchBox.value}`;
+  }
 
-searchButton.addEventListener("click", autoSearchDebounce, queryAddress);
-searchBoxValue.addEventListener("keyup", autoSearchDebounce);
+  autoSearchDebounce() {
+    clearTimeout(this.timeout);
+    this.timeout = setTimeout(async () => {
+      this.showLoader();
+      await searchResults.getNameAndSymbol(this.searchBox.value);
+      this.hideLoader();
+    }, 500);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const searchBox = document.getElementById("searchBox");
+  const searchButton = document.getElementById("searchButton");
+  const loader = document.getElementById("loading");
+
+  new SearchForm(searchBox, searchButton, loader);
+});
--- js/searchResults.js
+++ js/searchResults.js
@@ -1,86 +1,80 @@
-const baseUrl =
-  "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3";
+class SearchResults {
+  constructor(resultsDiv, baseUrl) {
+    this.resultsDiv = resultsDiv;
+    this.baseUrl = baseUrl;
+  }
 
-function highlightText(text, searchTerm) {
-  const regex = new RegExp(searchTerm, "gi");
-  return text.replace(regex, (match) => `<mark>${match}</mark>`);
-}
+  highlightText(text, query) {
+    const regex = new RegExp(`(${query})`, "gi");
+    return text.replace(regex, "<mark>$1</mark>");
+  }
 
-function getNameAndSymbol(name) {
-  let endpoint = `${baseUrl}/search?query=${name}&limit=10&exchange=NASDAQ`;
-  resultsDiv.innerHTML = "<h1>.your_search_results:</h1>";
-  return fetch(endpoint)
-    .then((response) => response.json())
-    .then((data) => {
+  async getNameAndSymbol(name) {
+    let endpoint = `${this.baseUrl}/search?query=${name}&limit=10&exchange=NASDAQ`;
+    this.resultsDiv.innerHTML = "<h1>.your_search_results:</h1>";
+    try {
+      const response = await fetch(endpoint);
+      const data = await response.json();
       let promises = [];
       for (let i = 0; i < 10; i++) {
         let symbol = data[i].symbol;
-        let imageUrl = "";
-        let promise = fetch(
-          `https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3/company/profile/${data[i].symbol}`
-        )
-          .then((response) => response.json())
-          .then((data) => {
-            let changesNegPoz =
-              data.profile.changesPercentage > 0
-                ? "text-success"
-                : "text-danger";
-            const pEls = resultsDiv.querySelectorAll("div p");
-            pEls[i].textContent = `Change: ${data.profile.changesPercentage}%`;
-            pEls[i].classList.add(changesNegPoz);
-            if (data.profile.changesPercentage > 0) {
-              pEls[i].innerHTML += ` <span style="color:green">&#9650;</span>`;
-            } else if (data.profile.changesPercentage < 0) {
-              pEls[i].innerHTML += ` <span style="color:red">&#9660;</span>`;
-            }
-            imageUrl = data.profile.image;
-
-            const imgEl = resultsDiv.querySelector(
-              `img[data-symbol="${symbol}"]`
-            );
-            if (imgEl) {
-              if (imageUrl) {
-                imgEl.src = imageUrl;
-              } else {
-                return;
-              }
-            }
-            const firstPEl = resultsDiv.querySelectorAll("p")[0];
-            if (firstPEl) {
-              firstPEl.innerHTML = `Changes Percentage: ${highlightText(
-                data.profile.changesPercentage + "%",
-                name
-              )}`;
-              firstPEl.classList.add(changesNegPoz);
-              const triangleIcon = document.createElement("span");
-              triangleIcon.classList.add("triangle");
-              triangleIcon.classList.add(
-                data.profile.changesPercentage > 0 ? "positive" : "negative"
-              );
-              firstPEl.appendChild(triangleIcon);
-            }
-            const nameEl = resultsDiv.querySelectorAll("div a")[i];
-            const nameText = nameEl.textContent;
-            nameEl.innerHTML = highlightText(nameText, name);
-          })
-          .catch((error) => (imgEl.style.display = "none"));
-        promises.push(promise);
-        resultsDiv.innerHTML += `
-        <div>
-          <a href="company.html?symbol=${symbol}">
-            ${highlightText(data[i].name, name)}, (${highlightText(
-          symbol,
-          name
-        )})
-            <img src="" data-symbol="${symbol}" alt="${
-          data[i].name
-        } icon" width="50" height="50">
-          </a><p></p>
-          <br>
-        </div>
-      `;
+        let highlightedName = this.highlightText(data[i].name, name);
+        let highlightedSymbol = this.highlightText(symbol, name);
+        promises.push(this.fetchSymbolData(symbol, i));
+        this.resultsDiv.innerHTML += `
+          <div>
+            <a href="company.html?symbol=${symbol}">
+              ${highlightedName}, (${highlightedSymbol})
+              <img src="" data-symbol="${symbol}" alt="${data[i].name} icon" width="50" height="50">
+            </a><p></p>
+            <br>
+          </div>
+        `;
       }
-      return Promise.all(promises);
-    })
-    .catch((error) => console.log(error));
+      await Promise.all(promises);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async fetchSymbolData(symbol, index) {
+    try {
+      const response = await fetch(`${this.baseUrl}/company/profile/${symbol}`);
+      const data = await response.json();
+      let changesNegPoz =
+        data.profile.changesPercentage > 0 ? "text-success" : "text-danger";
+      const pEls = this.resultsDiv.querySelectorAll("div p");
+      pEls[index].textContent = `Change: ${data.profile.changesPercentage}%`;
+      pEls[index].classList.add(changesNegPoz);
+      if (data.profile.changesPercentage > 0) {
+        pEls[index].innerHTML += ` <span style="color:green">&#9650;</span>`;
+      } else if (data.profile.changesPercentage < 0) {
+        pEls[index].innerHTML += ` <span style="color:red">&#9660;</span>`;
+      }
+      const imgEl = this.resultsDiv.querySelector(
+        `img[data-symbol="${symbol}"]`
+      );
+      if (imgEl) {
+        imgEl.src = data.profile.image;
+      }
+    } catch (error) {
+      const imgEl = this.resultsDiv.querySelector(
+        `img[data-symbol="${symbol}"]`
+      );
+      imgEl.style.display = "none";
+    }
+  }
 }
+
+const searchResults = new SearchResults(
+  document.getElementById("resultsDiv"),
+  "https://stock-exchange-dot-full-stack-course-services.ew.r.appspot.com/api/v3"
+);
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  document.querySelectorAll("img").forEach(function (img) {
+    img.onerror = function () {
+      this.style.display = "none";
+    };
+  });
+});
